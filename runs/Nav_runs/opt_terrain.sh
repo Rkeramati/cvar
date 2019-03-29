b1d194a2edc4fb31ba370ee2a02aa5d6144ea2cf
rkeramati <keramati@stanford.edu>
2019-03-28 17:25:12 -0700

    more runs.sh for Nav 2D

---
+++ runs/Nav_runs/opt_terrain.sh
@@ -0,0 +1,5 @@
+python run.py --name 'Navres/opt_mrp_c_10' --trial 5 --opt 10.0 --alpha 0.25 --env '2D' --num_episode 20000
+
+python run.py --name 'Navres/opt_mrp_c_5' --trial 5 --opt 5.0 --alpha 0.25 --env '2D' --num_episode 20000
+
+python run.py --name 'Navres/opt_mrp_c_2' --trial 5 --opt 2.0 --alpha 0.25 --env '2D' --num_episode 20000
