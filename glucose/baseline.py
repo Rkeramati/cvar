c50bcc5cdbbb829d1f2eb104acd7f72780f44f9d
rkeramati <keramati@stanford.edu>
2019-04-01 13:13:14 -0700

    added glucose

---
+++ glucose/baseline.py
@@ -0,0 +1,33 @@
+from simglucose.simulation.env import T1DSimEnv
+from simglucose.controller.basal_bolus_ctrller import BBController
+from simglucose.sensor.cgm import CGMSensor
+from simglucose.sensor.noise_gen import CGMNoise
+from simglucose.actuator.pump import InsulinPump
+from simglucose.patient.t1dpatient import T1DPatient
+from simglucose.simulation.scenario_gen import RandomScenario
+from simglucose.simulation.scenario import CustomScenario
+from simglucose.simulation.sim_engine import SimObj, sim, batch_sim
+from datetime import timedelta
+from datetime import datetime
+
+# specify start_time as the beginning of today
+now = datetime.now()
+start_time = datetime.combine(now.date(), datetime.min.time())
+
+# Specify results saving path
+path = './results'
+
+# Create a simulation environment
+patient = T1DPatient.withName('adolescent#002')
+sensor = CGMSensor.withName('Dexcom', seed=1)
+pump = InsulinPump.withName('Insulet')
+scenario = RandomScenario(start_time=start_time, seed=1)
+env = T1DSimEnv(patient, sensor, pump, scenario)
+
+# Create a controller
+controller = BBController()
+
+# Put them together to create a simulation object
+s1 = SimObj(env, controller, timedelta(days=1), animate=False, path=path)
+results1 = sim(s1)
+print(results1)
