6a95c804e263a8b63e92082362b09d04ebdef930
rkeramati <keramati@stanford.edu>
2019-05-08 17:17:06 -0700

    runs

--- glucose/runs/glucose4.sh
+++ glucose/runs/glucose4.sh
@@ -1,3 +1,3 @@
-python main.py --save_name results_c51/patient_5_seed_2_e_4_nd --hour 10 --seed 2 --num_episode 1000 --action_sigma 1.0 --ifCVaR 1 --alpha 0.25 --action_delay 5 --e_greedy 1 --patient 5 --e_greedy_option 4
+python main.py --save_name results_c51/patient_5_seed_2_e_4_nd --hour 10 --seed 2 --num_episode 20000 --action_sigma 1.0 --ifCVaR 1 --alpha 0.25 --action_delay 5 --e_greedy 1 --patient 5 --e_greedy_option 4
 
-python main.py --save_name results_c51/patient_5_seed_3_e_4_nd --hour 10 --seed 3 --num_episode 1000 --action_sigma 1.0 --ifCVaR 1 --alpha 0.25 --action_delay 5 --e_greedy 1 --patient 5 --e_greedy_option 4
+python main.py --save_name results_c51/patient_5_seed_3_e_4_nd --hour 10 --seed 3 --num_episode 20000 --action_sigma 1.0 --ifCVaR 1 --alpha 0.25 --action_delay 5 --e_greedy 1 --patient 5 --e_greedy_option 4
--- glucose/runs/glucose7.sh
+++ glucose/runs/glucose7.sh
@@ -1,3 +1,3 @@
-python main.py --save_name results_c51/patient_5_seed_2_e_3_d --hour 10 --seed 2 --num_episode 1000 --action_sigma 1.0 --ifCVaR 1 --alpha 0.25 --action_delay 5 --e_greedy 1 --patient 5 --e_greedy_option 1 --decay 3
+python main.py --save_name results_c51/patient_5_seed_2_e_3_d --hour 10 --seed 2 --num_episode 20000 --action_sigma 1.0 --ifCVaR 1 --alpha 0.25 --action_delay 5 --e_greedy 1 --patient 5 --e_greedy_option 1 --decay 3
 
-python main.py --save_name results_c51/patient_5_seed_3_e_3_d --hour 10 --seed 3 --num_episode 1000 --action_sigma 1.0 --ifCVaR 1 --alpha 0.25 --action_delay 5 --e_greedy 1 --patient 5 --e_greedy_option 1 --decay 3
+python main.py --save_name results_c51/patient_5_seed_3_e_3_d --hour 10 --seed 3 --num_episode 20000 --action_sigma 1.0 --ifCVaR 1 --alpha 0.25 --action_delay 5 --e_greedy 1 --patient 5 --e_greedy_option 1 --decay 3
