f1ce3d016dfbe86356643c60d7c6567c01d5fc3e
Rkeramati <ramtinkeramati@DNab404677.stanford.edu>
2019-05-15 11:02:53 -0700

    e-greedy runs

---
+++ glucose/runs_egreedy_p2/glucose7.sh
@@ -0,0 +1,2 @@
+python main.py --save_name results_c51/patient_2_seed_7_e --hour 10 --seed 7 --num_episode 20000 --action_sigma 1.0 --ifCVaR 1 --alpha 0.25 --action_delay 5 --e_greedy 1 --patient 2 --e_greedy_option 1 --decay 1
+
---
+++ glucose/runs_egreedy_p5/glucose9.sh
@@ -0,0 +1,2 @@
+python main.py --save_name results_c51/patient_5_seed_9_e --hour 10 --seed 9 --num_episode 20000 --action_sigma 1.0 --ifCVaR 1 --alpha 0.25 --action_delay 5 --e_greedy 1 --patient 5 --e_greedy_option 1 --decay 1
+
