33413e5a55a4e8aa3cf487b7785575a918480678
Rkeramati <ramtinkeramati@DNab4046a5.stanford.edu>
2019-05-16 15:15:17 -0700

    opt runs added

---
+++ glucose/runs_o2_p2/o2glucose2.sh
@@ -0,0 +1,2 @@
+python main.py --save_name results_c51/patient_2_seed_2_o2 --opt 0.2 --patient 2 --seed 2 --hour 10  --num_episode 20000 --action_sigma 1.0 --ifCVaR 1 --alpha 0.25 --action_delay 5  
+
