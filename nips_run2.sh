516734c41ace680156cc356c1ec072c6a1daaa84
rkeramati <keramati@stanford.edu>
2019-05-19 15:05:53 -0700

    more runs

---
+++ nips_run2.sh
@@ -0,0 +1,3 @@
+python e_greedy.py --name nips_res/mrp_eg1 --trial 15 --num_episode 20001 --option 1 --gamma 0.99
+python e_greedy.py --name nips_res/mrp_eg5 --trial 15 --num_episode 20001 --option 5 --gamma 0.99
+python e_greedy.py --name nips_res/mrp_eg8 --trial 15 --num_episode 20001 --option 8 --gamma 0.99
