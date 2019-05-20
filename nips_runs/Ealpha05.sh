e6732808061d5dc930d34647f7278188345955dc
Rkeramati <ramtinkeramati@DN51s5tr.SUNet>
2019-05-19 22:12:15 -0700

    more runs

---
+++ nips_runs/Ealpha05.sh
@@ -0,0 +1,3 @@
+python e_greedy.py --name nips_res/mrp_eg1_alpha05 --trial 15 --num_episode 20001 --option 1 --gamma 0.99 --alpha 0.5
+python e_greedy.py --name nips_res/mrp_eg5_alpha05 --trial 15 --num_episode 20001 --option 5 --gamma 0.99 --alpha 0.5
+python e_greedy.py --name nips_res/mrp_eg8_alpha05 --trial 15 --num_episode 20001 --option 8 --gamma 0.99 --alpha 0.5
---
+++ nips_runs/opt05alpha05.sh
@@ -0,0 +1,2 @@
+python run.py --name nips_res/mrp_opt05_alpha05 --trial 15 --opt 0.5 --alpha 0.5 --num_episode 20001 --gamma 0.99
+python run.py --name nips_res/mrp_opt25_alpha05 --trial 15 --opt 0.25 --alpha 0.5 --num_episode 20001 --gamma 0.99
