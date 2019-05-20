481a2965eefc1e761c3410819165a3eb70384680
Rkeramati <ramtinkeramati@DN51s5tr.SUNet>
2019-05-19 22:03:12 -0700

    more runs

---
+++ nips_runs/opt02alpha25.sh
@@ -0,0 +1,2 @@
+python run.py --name nips_res/mrp_opt02_alpha25 --trial 15 --opt 0.2 --alpha 0.25 --num_episode 20001 --gamma 0.99
+python run.py --name nips_res/mrp_opt01_alpha25 --trial 15 --opt 0.1 --alpha 0.25 --num_episode 20001 --gamma 0.99
