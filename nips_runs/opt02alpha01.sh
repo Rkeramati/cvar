9d8e407e8263ace70f7a8224e71a675ce3e64870
Rkeramati <ramtinkeramati@DN51s5p6.SUNet>
2019-05-20 22:49:08 -0700

    more runs

--- nips_runs/opt02alpha01.sh
+++ nips_runs/opt02alpha01.sh
@@ -1,2 +1,2 @@
-python run.py --name nips_res/mrp_opt02_alpha01 --trial 15 --opt 0.2 --alpha 0.1 --num_episode 20001 --gamma 0.99
-python run.py --name nips_res/mrp_opt01_alpha01 --trial 15 --opt 0.1 --alpha 0.1 --num_episode 20001 --gamma 0.99
+python run.py --name nips_res/mrp_opt05_alpha01 --trial 15 --opt 0.5 --alpha 0.1 --num_episode 20001 --gamma 0.99
+python run.py --name nips_res/mrp_opt25_alpha01 --trial 15 --opt 0.25 --alpha 0.1 --num_episode 20001 --gamma 0.99
